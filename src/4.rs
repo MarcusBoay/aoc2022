403cd5e4d0f9f50f1b9d5dd3e6a424d6f85979b9
Marcus Boay <marcusboay@hotmail.com>
2022-12-04 00:53:27 -0500

    refactor 4

--- src/4.rs
+++ src/4.rs
@@ -10,46 +10,32 @@ fn main() {
     let data = fs::read_to_string(filepath)
         .unwrap_or_else(|_| panic!("Unable to read file {}.", filepath.to_owned()));
 
-    let mut total_contained_pairs = 0;
-    let mut total_overlapping_pairs = 0;
-    for line in data.split('\n').map(String::from).collect::<Vec<String>>() {
-        // Final line.
-        if line.is_empty() {
-            break;
-        }
-
-        let pairs = line.split(',').map(String::from).collect::<Vec<String>>();
-        let pair1 = pairs[0]
-            .split('-')
-            .map(|x| String::from(x).parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-        let pair2 = pairs[1]
-            .split('-')
-            .map(|x| String::from(x).parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-        total_contained_pairs += is_contained(&pair1, &pair2);
-        total_overlapping_pairs += is_overlapping(&pair1, &pair2);
+    let mut contained_pairs = 0;
+    let mut overlapping_pairs = 0;
+    for line in data.trim().split('\n').collect::<Vec<&str>>() {
+        let pairs = line
+            .split(',')
+            .map(|x| {
+                x.split('-')
+                    .map(|x| x.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>()
+            })
+            .collect::<Vec<Vec<i32>>>();
+        contained_pairs += is_contained(&pairs[0], &pairs[1]);
+        overlapping_pairs += is_overlapping(&pairs[0], &pairs[1]);
     }
-    println!("Total contained pairs: {total_contained_pairs}");
-    println!("Total contained pairs: {total_overlapping_pairs}");
+    println!("Total contained pairs: {contained_pairs}");
+    println!("Total overlapping pairs: {overlapping_pairs}");
 }
 
 fn is_contained(pair1: &[i32], pair2: &[i32]) -> i32 {
-    if pair1[0] == pair2[0]
+    (pair1[0] == pair2[0]
         || pair1[1] == pair2[1]
         || (pair1[0] > pair2[0] && pair1[1] <= pair2[1])
-        || (pair2[0] > pair1[0] && pair2[1] <= pair1[1])
-    {
-        return 1;
-    }
-    0
+        || (pair2[0] > pair1[0] && pair2[1] <= pair1[1])) as i32
 }
 
 fn is_overlapping(pair1: &[i32], pair2: &[i32]) -> i32 {
-    if (pair1[0] >= pair2[0] && pair1[0] <= pair2[1])
-        || (pair2[0] >= pair1[0] && pair2[0] <= pair1[1])
-    {
-        return 1;
-    }
-    0
+    ((pair1[0] >= pair2[0] && pair1[0] <= pair2[1])
+        || (pair2[0] >= pair1[0] && pair2[0] <= pair1[1])) as i32
 }
